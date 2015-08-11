6e0a366b693d268e8dc0056ef60c679f5e940f6f
keiyakanno <keiya@kanno.com>
2015-08-11 10:52:05 +0000

    avatar upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150801091407) do
+ActiveRecord::Schema.define(version: 20150811104138) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150801091407) do
     t.datetime "updated_at"
     t.text     "profile"
     t.text     "region"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
